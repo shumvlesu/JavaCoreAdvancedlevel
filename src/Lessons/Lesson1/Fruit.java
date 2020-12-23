e54d4ef770a48a98e04bc0a1146b09a133a2a4ce
shumvlesu <sashumihin@gmail.com>
2020-12-23 18:07:24 +0300

    Ничего

--- src/Lessons/Lesson1/Fruit.java
+++ src/Lessons/Lesson1/Fruit.java
@@ -1,9 +1,11 @@
 package Lessons.Lesson1;
 
 public enum Fruit {
+
   ORANGE("Апельсин", 3), APPLE("Яблоко", 3), BANANA("Банан", 2), CHERRY("Вишня", 1);
-  private String russianTitle;
-  private int weight;
+
+  private final String russianTitle;
+  private final int weight;
 
   public String getRussianTitle() {
     return russianTitle;
