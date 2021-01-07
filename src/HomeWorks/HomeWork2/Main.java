b6a0d963ccb03be12703443a0d617071dd3c5e0d
shumvlesu <sashumihin@gmail.com>
2021-01-07 13:48:33 +0300

    ДЗ 2

--- src/HomeWorks/HomeWork2/Main.java
+++ src/HomeWorks/HomeWork2/Main.java
@@ -2,6 +2,8 @@ package HomeWorks.HomeWork2;
 
 public class Main {
 
+  public static final int MAX_SIZE = 4;
+
 //1. Напишите метод, на вход которого подается двумерный строковый массив размером 4х4,
 // при подаче массива другого размера необходимо бросить исключение MyArraySizeException.
 
@@ -11,9 +13,25 @@ public class Main {
 
 //3. В методе main() вызвать полученный метод, обработать возможные исключения MySizeArrayException и MyArrayDataException и вывести результат расчета.
 
+  public static void main(String[] args) {
+    String[][] myArray = new String[][]{{"1", "2", "3", "4"},
+                                        {"1", "2", "Ё", "4"},
+                                        {"1", "2", "3", "4"},
+                                        {"1", "2", "3", "4"}};
 
+    myMethod(myArray);
+  }
 
-
+  //1. Напишите метод, на вход которого подается двумерный строковый массив размером 4х4,
+  public static void myMethod(String[][] myArray) {
+    if (myArray[0].length > MAX_SIZE || myArray.length > MAX_SIZE) {
+      try {
+        throw new MyArraySizeException("Размер должен быть 4x4");
+      } catch (MyArraySizeException e) {
+        e.printStackTrace();
+      }
+    }
+  }
 
 
 }
---
+++ src/HomeWorks/HomeWork2/MyArraySizeException.java
@@ -0,0 +1,7 @@
+package HomeWorks.HomeWork2;
+
+public class MyArraySizeException extends Exception{
+  public MyArraySizeException(String message) {
+    super(message);
+  }
+}
