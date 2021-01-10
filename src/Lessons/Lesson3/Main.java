6bdd301dceabfdc0ad3d0de2621f7578d4ccadb6
sergey <sashumihin@gmail.com>
2021-01-10 13:18:30 +0300

    3 урок

--- src/Lessons/Lesson3/Main.java
+++ src/Lessons/Lesson3/Main.java
@@ -1,7 +1,14 @@
 package Lessons.Lesson3;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Main {
   public static void main(String[] args) {
-    
+    //Лучше использовать такое объявление
+    List<String> stringlist = new ArrayList<>();
+    //чем такое
+    //ArrayList<String> stringlist = new ArrayList<>();
+
   }
 }
