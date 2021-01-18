155b8e1ee9cb8bfc63e89fe0470874dc8bf73d5d
shumvlesu <sashumihin@gmail.com>
2021-01-18 17:04:02 +0300

    5 Урок

--- src/Lessons/Lesson5/Main.java
+++ src/Lessons/Lesson5/Main.java
@@ -1,7 +1,23 @@
 package Lessons.Lesson5;
 
+import java.util.concurrent.TimeUnit;
+
 public class Main {
-  public static void main(String[] args) {
+  public static void main(String[] args) throws InterruptedException {
+
+    Thread thread = new Thread(){
+      @Override
+      public void run() {
+        do {
+          System.out.println("Тик");
+        }while (!isInterrupted());
+      }
+    };
+
+    thread.start();
+    Thread.sleep(TimeUnit.SECONDS.toMillis(3));
+    thread.interrupt();
+
 
   }
 }
